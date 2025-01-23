a8b5b93a4473759bfe062681510c28f5830f12f0
gohlucas <xiaopangpang.118@gmail.com>
2025-01-23 16:15:08 +0800

    lvl3

--- src/main/java/Dexter.java
+++ src/main/java/Dexter.java
@@ -3,6 +3,25 @@ import java.util.Scanner;
 import java.util.List;
 
 public class Dexter {
+    public static class Task {
+        private String description;
+        private boolean isDone;
+
+        Task(String description) {
+            this.description = description;
+            this.isDone = false;
+        }
+
+        public void changeDoneStatus() {
+            this.isDone = !this.isDone;
+        }
+
+        @Override
+        public String toString() {
+            String mark = (this.isDone) ? "X" : " ";
+            return "[" + mark + "] " + this.description;
+        }
+    }
     public static void main(String[] args) {
         String greet = "\t____________________________________________________________\n"
         + "\tHello! I'm Dexter Morgan, ahem...The Bay Harbour Butcher\n"
@@ -11,13 +30,28 @@ public class Dexter {
         + "\t____________________________________________________________\n";
 
         System.out.println(greet);
-        List<String> lst = new ArrayList<>();
+        List<Task> lst = new ArrayList<>();
         Scanner scan = new Scanner(System.in);
         String altReply = "\t____________________________________________________________\n"
                 + "\tBye, Hope to see you again soon!\n"
                 + "\t____________________________________________________________\n";
         while(true) {
-            String input = scan.nextLine();
+            String input = scan.next();
+
+            if (input.equals("mark") || input.equals("unmark")) {
+                int j = Integer.parseInt(scan.next()) - 1;
+                Task a = lst.get(j);
+                a.changeDoneStatus();
+                String reply = input.equals("mark") ? "Nice! I've marked this task as done:\n"
+                        : "Ok, I've marked this task as not done yet:\n";
+                String s = "\t____________________________________________________________\n"
+                        + "\t" + reply + "\n"
+                        + "\t" + a.toString() + "\n"
+                        + "\t____________________________________________________________\n";
+                System.out.println(s);
+                continue;
+            }
+            input += scan.nextLine();
             if (input.equals("bye")) {
                 System.out.println(altReply);
                 break;
@@ -30,7 +64,7 @@ public class Dexter {
                 System.out.println("\t____________________________________________________________\n");
                 continue;
             }
-            lst.add(input);
+            lst.add(new Task(input));
             String reply = "\t____________________________________________________________\n"
             + "\tadded: " + input + "\n"
             + "\t____________________________________________________________\n";
