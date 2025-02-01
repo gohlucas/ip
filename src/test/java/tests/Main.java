76d10519587be81a4869edac5292852925c01bf8
gohlucas <xiaopangpang.118@gmail.com>
2025-02-01 16:56:12 +0800

    temp

--- src/test/java/tests/Main.java
+++ src/test/java/tests/Main.java
@@ -1,2 +1,19 @@
-package tests;public class Main {
+package tests;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class Main {
+    public static void hehe() {
+//        try {
+            File f = new File("Data.txt");
+            System.out.println("Success");
+//        } catch (IOException e) {
+//            System.out.println("sed");
+//        }
+    }
+    public static void main(String[] args) {
+        Main.hehe();
+    }
 }
